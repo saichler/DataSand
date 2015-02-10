22e3dc5b8daf4a1d9c4ddeb891b5ff9bd1c6ac04
Sharon Aicler <saichler@cisco.com>
2015-02-10 14:36:02 -0800

    Remove inital project as it did  not have the correct org
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- store/src/main/java/org/opendaylight/datasand/store/SubPojoList.java
+++
@@ -1,7 +0,0 @@
-package org.opendaylight.datasand.store;
-
-public class SubPojoList {
-    private String name = null;
-    public void setName(String n){this.name=n;}
-    public String getName(){return this.name;}
-}
