8e62c42d7021a62db26c57bc0262bddc1d4d4ab0
Sharon Aicler <saichler@gmail.com>
2015-02-16 10:05:38 -0800

    Add deploy script
    Signed-off-by: Sharon Aicler <saichler@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Sharon Aicler"
+git config --global user.email saichler@gmail.com
+git config --global author.name "Sharon Aicler"
+git config --global author.email "saichler@gmail.com"
+mvn clean deploy -DskipTests=true
