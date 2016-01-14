cdf2f866807fa59a2642a9f8265a138d4c912f32
Sharon Aicler <saichler@cisco.com>
2016-01-14 08:25:00 -0800

    Test
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- store/src/main/java/org/datasand/store/DataStore.java
+++ store/src/main/java/org/datasand/store/DataStore.java
@@ -1,11 +1,14 @@
 package org.datasand.store;
 
+import org.datasand.codec.HierarchyBytesArray;
+
 /**
  * Created by root on 1/12/16.
  */
 public class DataStore {
 
     public void put(Object key,Object object){
+        HierarchyBytesArray ba = new HierarchyBytesArray();
     }
 
     public Object get(Object key){
