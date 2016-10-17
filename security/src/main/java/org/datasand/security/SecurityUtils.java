23a3a2163713caca13877930ce2b1b8334b83e8e
Sharon Aicler <saichler@cisco.com>
2016-10-17 09:58:10 -0700

    Replace VLogger with logback
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- security/src/main/java/org/datasand/security/SecurityUtils.java
+++ security/src/main/java/org/datasand/security/SecurityUtils.java
@@ -31,15 +31,19 @@ import javax.crypto.CipherOutputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
-import org.datasand.codec.VLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author - Sharon Aicler (saichler@gmail.com)
  */
 public class SecurityUtils {
-    private static byte[] iv = { 0, 4, 0, 0, 6, 81, 0, 8, 71, 86, 5, 0, 0, 43, 0, 1};
+    private static final Logger LOG = LoggerFactory.getLogger(SecurityUtils.class);
+    private static byte[] iv = {0, 4, 0, 0, 6, 81, 0, 8, 71, 86, 5, 0, 0, 43, 0, 1};
     protected static IvParameterSpec ivspec = new IvParameterSpec(iv);
+
     //Encrypt a byte array using the given key.
-    public static byte[] encrypt(byte[] dataToEncode,SecretKey key){
+    public static byte[] encrypt(byte[] dataToEncode, SecretKey key) {
         try {
             Cipher cr = Cipher.getInstance("AES/CFB8/NoPadding");
             cr.init(Cipher.ENCRYPT_MODE, key, ivspec);
@@ -49,13 +53,13 @@ public class SecurityUtils {
             out.close();
             return bout.toByteArray();
         } catch (Exception err) {
-            VLogger.error("Failed to encrypt byte array", err);
+            LOG.error("Failed to encrypt byte array", err);
             return null;
         }
     }
 
     //Decrypt a byte array using the given key
-    public static byte[] decrypt(byte[] dataToDecrypt,SecretKey key){
+    public static byte[] decrypt(byte[] dataToDecrypt, SecretKey key) {
         try {
             Cipher cr = Cipher.getInstance("AES/CFB8/NoPadding");
             cr.init(Cipher.DECRYPT_MODE, key, ivspec);
@@ -66,144 +70,144 @@ public class SecurityUtils {
             in.close();
             return data;
         } catch (Exception err) {
-            VLogger.error("Failed To Decrypt byte array.", err);
+            LOG.error("Failed To Decrypt byte array.", err);
             return null;
         }
     }
 
-    public static final SecretKey generateSecretKey(){
+    public static final SecretKey generateSecretKey() {
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance("AES");
             keyGen.init(128);
             return keyGen.generateKey();
         } catch (NoSuchAlgorithmException e) {
-            VLogger.error("Failed to generate a secret key.",e);
+            LOG.error("Failed to generate a secret key.", e);
             return null;
         }
     }
 
-    public static final KeyPair generateRSAKeys(){
-    	try{
-    	    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-    	    keyPairGenerator.initialize(2048);
-    	    return keyPairGenerator.genKeyPair();
-    	  }catch(Exception e){
-            VLogger.error("Failed to generate a RSA key.",e);
+    public static final KeyPair generateRSAKeys() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.genKeyPair();
+        } catch (Exception e) {
+            LOG.error("Failed to generate a RSA key.", e);
             return null;
-    	}
+        }
     }
 
-    public static final byte[] encryptRSA(byte dataToEncrypt[], PublicKey publicKey){
+    public static final byte[] encryptRSA(byte dataToEncrypt[], PublicKey publicKey) {
         //if the data is larger than 117 bytes. we need to encrypt it in two blocks
         //we divide to 110 per block
         byte blockCount = 0;
-        if(dataToEncrypt.length>110){
-            blockCount = (byte)(dataToEncrypt.length/110+1);
+        if (dataToEncrypt.length > 110) {
+            blockCount = (byte) (dataToEncrypt.length / 110 + 1);
         }
-    	try{
-    	    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    	    cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-    	    if(blockCount==0){
-    	        byte encData[] = cipher.doFinal(dataToEncrypt);
-    	        byte[] result = new byte[encData.length+1];
-    	        result[0] = (byte)encData.length;
-    	        System.arraycopy(encData, 0, result, 1, encData.length);
-    	        return result;
-    	    }else{
-    	        List<byte[]> blocks = new LinkedList<>();
-    	        for(int i=0;i<blockCount;i++){
-    	            if(dataToEncrypt.length-i*110>=110){
-    	                byte data[] = new byte[110];
-    	                System.arraycopy(dataToEncrypt, i*110, data, 0, data.length);
-    	                byte encData[] = cipher.doFinal(data);
-    	                blocks.add(encData);
-    	            }else{
-                        byte data[] = new byte[dataToEncrypt.length-i*110];
-                        System.arraycopy(dataToEncrypt, i*110, data, 0, data.length);
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            if (blockCount == 0) {
+                byte encData[] = cipher.doFinal(dataToEncrypt);
+                byte[] result = new byte[encData.length + 1];
+                result[0] = (byte) encData.length;
+                System.arraycopy(encData, 0, result, 1, encData.length);
+                return result;
+            } else {
+                List<byte[]> blocks = new LinkedList<>();
+                for (int i = 0; i < blockCount; i++) {
+                    if (dataToEncrypt.length - i * 110 >= 110) {
+                        byte data[] = new byte[110];
+                        System.arraycopy(dataToEncrypt, i * 110, data, 0, data.length);
                         byte encData[] = cipher.doFinal(data);
                         blocks.add(encData);
-    	            }
-    	        }
-    	        byte result[] = new byte[256*blocks.size()+1];
-    	        result[0] = (byte)blocks.size();
-    	        int pos = 1;
-    	        for(byte[] d:blocks){
-    	            System.arraycopy(d,0, result, pos, d.length);
-    	            pos+=d.length;
-    	        }
-    	        return result;
-    	    }
-    	    
-    	}catch(Exception e){
-    		VLogger.error("Failed to encrypt RSA",e);
-    	}
-    	return null;
+                    } else {
+                        byte data[] = new byte[dataToEncrypt.length - i * 110];
+                        System.arraycopy(dataToEncrypt, i * 110, data, 0, data.length);
+                        byte encData[] = cipher.doFinal(data);
+                        blocks.add(encData);
+                    }
+                }
+                byte result[] = new byte[256 * blocks.size() + 1];
+                result[0] = (byte) blocks.size();
+                int pos = 1;
+                for (byte[] d : blocks) {
+                    System.arraycopy(d, 0, result, pos, d.length);
+                    pos += d.length;
+                }
+                return result;
+            }
+
+        } catch (Exception e) {
+            LOG.error("Failed to encrypt RSA", e);
+        }
+        return null;
     }
 
-    public static final byte[] decryptRSA(byte dataToDecrypt[],PrivateKey privateKey){
-        try{
+    public static final byte[] decryptRSA(byte dataToDecrypt[], PrivateKey privateKey) {
+        try {
             Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
             cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            if(dataToDecrypt[0]==0){
-                byte data[] = new byte[dataToDecrypt.length-1];
+            if (dataToDecrypt[0] == 0) {
+                byte data[] = new byte[dataToDecrypt.length - 1];
                 System.arraycopy(dataToDecrypt, 1, data, 0, data.length);
                 return cipher.doFinal(data);
-            }else{
+            } else {
                 int blockCount = dataToDecrypt[0];
                 List<byte[]> data = new ArrayList<byte[]>(blockCount);
                 int len = 0;
-                for(int i=0;i<blockCount;i++){
+                for (int i = 0; i < blockCount; i++) {
                     byte encData[] = new byte[256];
-                    System.arraycopy(dataToDecrypt, 256*i+1, encData,0, 256);
+                    System.arraycopy(dataToDecrypt, 256 * i + 1, encData, 0, 256);
                     byte decData[] = cipher.doFinal(encData);
-                    len+=decData.length;
+                    len += decData.length;
                     data.add(decData);
                 }
                 byte result[] = new byte[len];
                 int pos = 0;
-                for(byte d[]:data){
-                    System.arraycopy(d, 0, result,pos, d.length);
-                    pos+=d.length;
+                for (byte d[] : data) {
+                    System.arraycopy(d, 0, result, pos, d.length);
+                    pos += d.length;
                 }
                 return result;
             }
-        }catch(Exception err){
-            VLogger.error("Failed to decrypt RSA",err);
+        } catch (Exception err) {
+            LOG.error("Failed to decrypt RSA", err);
         }
         return null;
     }
 
-    public static byte[] getRSAPublicKey(PublicKey key){
-        try{
-        	return key.getEncoded();
-        }catch(Exception e){
-        	e.printStackTrace();
+    public static byte[] getRSAPublicKey(PublicKey key) {
+        try {
+            return key.getEncoded();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    public static PublicKey getRSAPublicKey(byte[] data){
-    	try {
-			return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(data));
-		} catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	return null;
+    public static PublicKey getRSAPublicKey(byte[] data) {
+        try {
+            return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(data));
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public static byte[] getSecretKey(SecretKey key){
+    public static byte[] getSecretKey(SecretKey key) {
         ObjectOutputStream bout = null;
-        try{
+        try {
             ByteArrayOutputStream baout = new ByteArrayOutputStream();
             bout = new ObjectOutputStream(new DataOutputStream(baout));
             bout.writeObject(key);
             bout.flush();
             return baout.toByteArray();
-        }catch(Exception e){
-            
-        }finally{
-            if(bout!=null)
+        } catch (Exception e) {
+
+        } finally {
+            if (bout != null)
                 try {
                     bout.close();
                 } catch (IOException e) {
@@ -214,16 +218,16 @@ public class SecurityUtils {
         return null;
     }
 
-    public static SecretKey getSecretKey(byte[] data){
+    public static SecretKey getSecretKey(byte[] data) {
         ObjectInputStream in = null;
-        try{
+        try {
             ByteArrayInputStream bin = new ByteArrayInputStream(data);
             in = new ObjectInputStream(new DataInputStream(bin));
-            return (SecretKey)in.readObject();
-        }catch(Exception e){
-            
-        }finally{
-            if(in!=null)
+            return (SecretKey) in.readObject();
+        } catch (Exception e) {
+
+        } finally {
+            if (in != null)
                 try {
                     in.close();
                 } catch (IOException e) {
